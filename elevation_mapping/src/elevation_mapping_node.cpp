bfdd63b0b025db4a5db791535630b6b09ddcb7b6
Ilyass Taouil <itaouil95@gmail.com>
2021-05-19 12:40:01 +0200

    Codebase exploration.

--- elevation_mapping/src/elevation_mapping_node.cpp
+++ elevation_mapping/src/elevation_mapping_node.cpp
@@ -10,13 +10,22 @@
 #include "elevation_mapping/ElevationMapping.hpp"
 
 int main(int argc, char** argv) {
+  // Initialize ROS node
   ros::init(argc, argv, "elevation_mapping");
+
+  // Create node handle which is passed
+  // as a parameter to the ElevationMapping class
   ros::NodeHandle nodeHandle("~");
+
+  // Elevation mapping class object is created
   elevation_mapping::ElevationMapping elevationMap(nodeHandle);
 
-  // Spin
+  // Async spinner whose threads can be defined as a parameter
   ros::AsyncSpinner spinner(nodeHandle.param("num_callback_threads", 1));  // Use n threads
+
+  // Async spinner is started
   spinner.start();
+  
   ros::waitForShutdown();
   return 0;
 }
